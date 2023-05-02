5c70938c683fdd5a051c55498d892ebfe0de18dc
WhoIsStanley <stanleychan246@gmail.com>
2023-05-02 18:21:23 +0800

    2/5/23 1820 3-"Vue setup"

--- src/main.js
+++ src/main.js
@@ -1,17 +1,20 @@
 // libaray
 import { Client, Events, GatewayIntentBits } from 'discord.js';
+import vueinit from '@/core/vue';
 import dotenv from 'dotenv';
 
+vueinit();
 dotenv.config();
 
 // Create a new client instance
 const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-// When the client is ready, run this code (only once)
-// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+// 'c' = event parameter to keep it separate from the already defined 'client'
 client.once(Events.ClientReady, c => {
 	console.log(`Bot Ready! Logged in as ${c.user.tag}`);
 });
+
+
 
 // Log in to Discord with your client's token
 client.login(process.env.TOKEN);
